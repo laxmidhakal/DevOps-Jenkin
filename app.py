c265fd1d7f8e4807438b4aa26fba19c295dce7dd
laxmi dhakal <laxmidhakal159@gmail.com>
2025-02-04 19:29:42 +0545

    testing docker compose

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, laxmi dhakal this is your python app runing on jenkins server"
+    return "Hello, laxmi this is your python app runing on jenkins server"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
